31452e5ed94253d6e457f5e06cd58325dab62cc6
Dr Kshitij Kumar <kkumar559@gmail.com>
2020-05-09 13:05:03 +0530

    added Procfile req and runtime.txt

--- covidsite/covidsite/settings.py
+++ covidsite/covidsite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '++0mxmj$fvqusr0a_bn#jt245r$t*dq@amazpyfk$($4+pd+db'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['mycovidsite.herokuapp.com','127.0.0.1']
 
 
 # Application definition
